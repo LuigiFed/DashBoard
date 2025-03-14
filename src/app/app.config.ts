d3def1d649c5ddbeec43c0253640151fb2699ef4
LuigiFed <luigi.federico@gmx.de>
2025-03-14 14:15:33 +0100

    new commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+//import Aura from '@primeng/themes/aura';
 import { CommonModule } from '@angular/common';
 import { ServerModule } from '@angular/platform-server';
 
@@ -22,7 +22,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-         preset: Aura
+        // preset: Aura
         }
     }),
     CommonModule,
